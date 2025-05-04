95f85f59badedaeda136cd3faa6a213179beafb6
Victor Kauan <victor.kauan@moldsoft.email>
2025-05-04 11:39:48 -0300

    :sparkles: feat: Add onClick property to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,10 +3,11 @@ import style from  "./Button.module.scss"
 
 export default class Button extends React.Component<{
   type?: "button" | "submit" | "reset",
+  onClick?: () => void
   children: React.ReactNode
 }> {
   render() {
-    const { type = "button" } = this.props
-    return <button className={style.button} type={type}>{this.props.children}</button>;
+    const { type = "button", onClick } = this.props
+    return <button className={style.button} type={type} onClick={onClick}>{this.props.children}</button>;
   }
 }
