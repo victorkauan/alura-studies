531e4146882ee21330ced53c918e9ac777f6015a
Victor Kauan <victor.kauan@moldsoft.email>
2025-05-04 10:14:16 -0300

    :sparkles: feat: Add type property to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,12 @@
 import React from "react"
 import style from  "./Button.module.scss"
 
-export default class Button extends React.Component<React.HTMLAttributes<HTMLButtonElement>> {
+export default class Button extends React.Component<{
+  type?: "button" | "submit" | "reset",
+  children: React.ReactNode
+}> {
   render() {
-    return <button className={style.button}>{this.props.children}</button>;
+    const { type = "button" } = this.props
+    return <button className={style.button} type={type}>{this.props.children}</button>;
   }
 }
