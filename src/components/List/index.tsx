6eeef0270b39f7e87784b30bbb8fe612459c115b
Victor Kauan <victor.kauan@moldsoft.email>
2025-05-03 21:40:12 -0300

    :sparkles: feat: Render list items dynamically

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,16 +1,20 @@
 export default function List() {
+  const tasks = [
+    { name: 'JavaScript', time: '00:30:00' },
+    { name: 'React', time: '01:00:00' },
+    { name: 'TypeScript', time: '01:30:00' }
+  ]
+
   return (
     <aside>
       <h2>Studies of the day</h2>
       <ul>
-        <li>
-          <h3>JavaScript</h3>
-          <span>00:30:00</span>
-        </li>
-        <li>
-          <h3>React</h3>
-          <span>01:00:00</span>
-        </li>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <h3>{task.name}</h3>
+            <span>{task.time}</span>
+          </li>
+        ))}
       </ul>
     </aside>
   );
